42fdde42ac41753eb4e0e03dfbaa6608297780d2
Josh Lasac <100702615+joshlsc@users.noreply.github.com>
2026-01-30 08:33:13 +0800

    Remove Test screen from HomeStackNavigator

--- src/navigation/HomeStackScreen.jsx
+++ src/navigation/HomeStackScreen.jsx
@@ -3,7 +3,6 @@ import HomeScreen from '../screens/HomeScreen'
 import DetailsScreen from '../screens/DetailsScreen'
 import ProductsScreen from '../screens/ProductsScreen'
 import StoreScreen from '../screens/StoreScreen'
-import Test from '../screens/Test'
 import TaskManager from '../screens/TaskManager'
 
 const HomeStack = createNativeStackNavigator()
@@ -15,7 +14,6 @@ export default function ProfileStackScreen() {
       <HomeStack.Screen name="Details" component={DetailsScreen} />
       <HomeStack.Screen name="Products" component={ProductsScreen} />
       <HomeStack.Screen name="Store" component={StoreScreen} />
-      <HomeStack.Screen name="Test" component={Test} />
       <HomeStack.Screen name="TaskManager" component={TaskManager} />
     </HomeStack.Navigator>
   )
